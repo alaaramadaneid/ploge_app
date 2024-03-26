0e57fb3b14288f75acba6449b613991dd53cc595
alaa <aedalaa36@gmail.com>
2024-03-26 07:50:52 +0100

    style.sass

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,26 +6,26 @@ const Home = () => {
     {
       id:1,
       title:"alaa rmjfg dfjsfhof ffsrfze jhzei grzlkef sgty tgrrzr srysr ",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
-      img:"https://placehold.co/150x200/png",
+      desc: "Lorem ipsum dolor sit amet conseLorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.ctetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
+      img:"https://placehold.co/400x600/png",
     },
     {
       id:2,
       title:"alaa rmjfg dfjsfhof ffsrfze jhzei grzlkef sgty tgrrzr srysr ",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
-      img:"https://placehold.co/150x200/png",
+      desc: "Lorem ipsum doloLorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.r sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
+      img:"https://placehold.co/400x600/png",
     },
     {
       id:3,
       title:"alaa rmjfg dfjsfhof ffsrfze jhzei grzlkef sgty tgrrzr srysr ",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
-      img:"https://placehold.co/150x200/png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
+      img:"https://placehold.co/400x600/png",
     },
     {
       id:4,
       title:"alaa rmjfg dfjsfhof ffsrfze jhzei grzlkef sgty tgrrzr srysr ",
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
-      img:"https://placehold.co/150x200/png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad dolore, hic obcaecati perspiciatis id beatae ratione, harum praesentium laborum molestiae consectetur odio enim. Dicta, distinctio.",
+      img:"https://placehold.co/400x600/png",
     },
   ]
   return (
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Edit from '../img/edit.png'
+import Delet from '../img/delet.png'
 
 const Single = () => {
   return (
-    <div>
-            <h1>alaa</h1>
+    <div className='single'>
+      <div className="content">  
+      <img src="https://placehold.co/1024x700/png" alt="" />
+      <div className="user">
+      <img src="https://placehold.co/200x400/png" alt="" />
+      <div className="edit" to={`/write?edit=2`}>
+        <img src={Edit} alt=''/>
+        <img src={Delet} alt=''/>
+      </div>
+      </div>
+      <div className="info">
+        <span>John</span>
+        <p>Posted 2 days ago</p>
+      </div>
+     
+</div>
+      <div className="menu">m</div>
+            
 
     </div>
   )
